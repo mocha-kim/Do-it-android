2891f2679c25553b01688b528db2ac013ade7f9e
mocha-kim <jsg2804@gmail.com>
2020-07-21 18:15:41 +0900

    SampleWidget reupload

---
+++ SampleWidget/app/src/main/java/org/mochakim/samplewidget/MainActivity.java
@@ -0,0 +1,14 @@
+package org.mochakim.samplewidget;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button);
+    }
+}
