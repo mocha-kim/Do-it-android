04266aa45012c3ba1228b2530263e1bebed35701
mocha-kim <jsg2804@gmail.com>
2020-07-27 20:57:13 +0900

    Sample Call Intent

---
+++ SampleProjects/SampleCallIntent/app/src/main/java/org/mochakim/samplecallintent/MainActivity.java
@@ -0,0 +1,48 @@
+package org.mochakim.samplecallintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_MENU = 101;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String data = editText.getText().toString();
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(data));
+                startActivity(intent);
+            }
+        });
+
+        Button button2 = findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                ComponentName name = new ComponentName("org.mochakim.samplecallintent",
+                        "org.mochakim.samplecallintent.MenuActivity");
+                intent.setComponent(name);
+                startActivityForResult(intent, 101);
+            }
+        });
+    }
+
+}
