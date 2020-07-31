d0b8a38e834e4b38db0f16b8ff6698bf84c7b50e
mocha-kim <jsg2804@gmail.com>
2020-07-31 20:31:18 +0900

    Sample Action Bar1

---
+++ SampleProjects/05/SampleActionBar1/app/src/main/java/org/mochakim/actionbar/MainActivity.java
@@ -0,0 +1,37 @@
+package org.mochakim.actionbar;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActionBar abar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        abar = getSupportActionBar();
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                abar.setLogo(R.drawable.home);
+                abar.setDisplayOptions(ActionBar.DISPLAY_SHOW_HOME|ActionBar.DISPLAY_USE_LOGO);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
