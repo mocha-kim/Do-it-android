f936671da77cdae90556cab3d2bc2c4286aa18fd
mocha-kim <jsg2804@gmail.com>
2020-08-07 18:48:24 +0900

    Sample Receiver

---
+++ SampleProjects/06/SampleReceiver/app/src/main/java/org/mochakim/receiver/MainActivity.java
@@ -0,0 +1,41 @@
+package org.mochakim.receiver;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.pedro.library.AutoPermissions;
+import com.pedro.library.AutoPermissionsListener;
+
+import javax.security.auth.AuthPermission;
+
+public class MainActivity extends AppCompatActivity implements AutoPermissionsListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AutoPermissions.Companion.loadAllPermissions(this, 101);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        AutoPermissions.Companion.parsePermissions(this, requestCode, permissions, this);
+    }
+
+    @Override
+    public void onDenied(int requestCode, @NonNull String[] permissions) {
+        Toast.makeText(this, "permissions denied : " + permissions.length,
+                        Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onGranted(int requestCode, @NonNull String[] permissions) {
+        Toast.makeText(this, "permissions granted : " + permissions.length,
+                Toast.LENGTH_LONG).show();
+    }
+}
