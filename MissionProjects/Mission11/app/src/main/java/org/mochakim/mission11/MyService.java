18f1cf7dc5e9c9017277c77ab96276ef182d0199
mocha-kim <jsg2804@gmail.com>
2020-08-11 23:11:53 +0900

    Mission11

---
+++ MissionProjects/Mission11/app/src/main/java/org/mochakim/mission11/MyService.java
@@ -0,0 +1,39 @@
+package org.mochakim.mission11;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MyService extends Service {
+    public MyService() {
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (intent == null) {
+            return Service.START_STICKY;
+        } else {
+            processCommand(intent);
+        }
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    public void processCommand(Intent intent) {
+        String command = intent.getStringExtra("command");
+        String text = intent.getStringExtra("text");
+
+        Intent showIntent = new Intent(getApplicationContext(), MainActivity.class);
+        showIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                            Intent.FLAG_ACTIVITY_SINGLE_TOP |
+                            Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        showIntent.putExtra("command", "show");
+        showIntent.putExtra("text", text);
+        startActivity(showIntent);
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
