bad4d0c4ee82bd71486393eb9bd1be0f6ffd2c0a
mocha-kim <jsg2804@gmail.com>
2020-08-13 18:52:24 +0900

    Sample Recycler View2

---
+++ SampleProjects/07/SampleRecyclerView2/app/src/main/java/org/mochakim/recyclerview/MainActivity.java
@@ -0,0 +1,50 @@
+package org.mochakim.recyclerview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    PersonAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new PersonAdapter();
+
+        adapter.addItem(new Person("김민수", "010-1000-1000"));
+        adapter.addItem(new Person("김하늘", "010-2000-2000"));
+        adapter.addItem(new Person("홍길동", "010-3000-3000"));
+        adapter.addItem(new Person("차학연", "010-4000-4000"));
+        adapter.addItem(new Person("정택운", "010-5000-5000"));
+        adapter.addItem(new Person("이재환", "010-6000-6000"));
+        adapter.addItem(new Person("김원식", "010-7000-7000"));
+        adapter.addItem(new Person("한상혁", "010-8000-8000"));
+
+        recyclerView.setAdapter(adapter);
+
+        adapter.setOnItemClickListener(new OnPersonItemClickListener() {
+            @Override
+            public void onItemClick(PersonAdapter.ViewHolder holder, View view, int position) {
+                Person item = adapter.getItem(position);
+                Toast.makeText(getApplicationContext(), "아이템 선택됨 : " + item.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
